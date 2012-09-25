8828b165164601519c50fbba173156adf4ea27d1
Thomas Parslow <tom@almostobsolete.net>
2012-09-25 18:52:30 +0100

    Wasn't passing aws access keys from layer2 of swf api to layer1 properly

--- boto/swf/layer2.py
+++ boto/swf/layer2.py
@@ -28,12 +28,13 @@ class Layer2(object):
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None,
                  is_secure=True, port=None, proxy=None, proxy_port=None,
                  debug=0, session_token=None, region=None, layer1=None):
+
         # We support passing in the layer1 to aid testing
         if layer1 is not None:
             self.layer1 = layer1
         else:
             self.layer1 = Layer1(
-                aws_secret_access_key, aws_secret_access_key, is_secure, port,
+                aws_access_key_id, aws_secret_access_key, is_secure, port,
                 proxy, proxy_port, debug, session_token, region)
             
     def get_domain(self, name):
